adbc0404781ee6728df26fc3a1069039ea6ed4cb
Lowe Thiderman <lowe.thiderman@gmail.com>
2014-03-22 11:12:46 +0100

    Add loading of dependencies from requirements.txt

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
+import sys
+import os
 from setuptools import setup
 
-install_requires = [
-    'docopt',
-    'tabulate',
-]
+
+def load_requirements():
+    filename = os.path.join(os.path.dirname(sys.argv[0]), 'requirements.txt')
+
+    with open(filename) as handle:
+        lines = (line.strip() for line in handle)
+        return [line for line in lines if line and not line.startswith("#")]
 
 setup(
     name='ngxtop',
@@ -13,7 +18,7 @@ setup(
     author_email='lebinh.it@gmail.com',
     description=('Real-time metrics for nginx server'),
     license='MIT',
-    install_requires=install_requires,
+    install_requires=load_requirements(),
     packages=['ngxtop'],
     zip_safe=False,
     entry_points={
