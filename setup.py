37f5a4c071a7e295172635f7d894f9bbf1800998
ducla <ducla@uiza.io>
2019-07-26 23:38:35 +0700

    add non cache column

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='ngxtop',
-    version='0.1.1',
+    version='0.1.2',
     description='Real-time metrics for nginx server',
     long_description=open('README.rst').read(),
     license='MIT',
