cff2cc6c7eda4329f5c9b5bd5f754aa5aafb7669
ducla <ducla@uiza.io>
2019-07-26 23:36:26 +0700

    fix: wrong query

--- ngxtop/ngxtop.py
+++ ngxtop/ngxtop.py
@@ -99,7 +99,7 @@ DEFAULT_QUERIES = [
        count(CASE WHEN status_type = 2 THEN 1 END) AS '2xx',
        count(CASE WHEN status_type = 3 THEN 1 END) AS '3xx',
        count(CASE WHEN status_type = 4 THEN 1 END) AS '4xx',
-       count(CASE WHEN status_type = 5 THEN 1 END) AS '5xx'
+       count(CASE WHEN status_type = 5 THEN 1 END) AS '5xx',
        count(CASE WHEN cache_status = 1 THEN 1 END) as 'cached'
      FROM log
      GROUP BY %(--group-by)s
