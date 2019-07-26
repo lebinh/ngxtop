e02b02a42c1a122385c0d02aa3421394fd2cd9dc
ducla <ducla@uiza.io>
2019-07-26 18:12:15 +0700

    fix wrong field name

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='ngxtop',
-    version='0.0.4',
+    version='0.0.5',
     description='Real-time metrics for nginx server',
     long_description=open('README.rst').read(),
     license='MIT',
